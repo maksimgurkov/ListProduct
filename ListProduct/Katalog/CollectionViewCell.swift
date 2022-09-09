0f2f5392c5bc8cb84722dfbe848cf11c28135604
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-09-09 14:54:48 +0300

    collectionView

---
+++ ListProduct/Katalog/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  ListProduct
+//
+//  Created by Максим Гурков on 09.09.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imagedoor: UIImageView!
+    @IBOutlet weak var nameDoorLabel: UILabel!
+}
---
+++ ListProduct/Katalog/KatalogDoorsCollectionViewController.swift
@@ -0,0 +1,78 @@
+//
+//  KatalogDoorsCollectionViewController.swift
+//  ListProduct
+//
+//  Created by Максим Гурков on 09.09.2022.
+//
+
+import UIKit
+
+class KatalogDoorsCollectionViewController: UICollectionViewController {
+    
+    var doors = Door.forDoors()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 5
+//    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return doors.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "katalog", for: indexPath) as! CollectionViewCell
+        let door = doors[indexPath.row]
+        cell.nameDoorLabel.text = door.nameDoor
+        cell.imagedoor.image = UIImage(named: door.nameDoor)
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+
+    
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
